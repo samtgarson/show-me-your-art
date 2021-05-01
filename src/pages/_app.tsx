c40f787f72e04e378356bd572ee37c842f65bf69
Sam Garson <samtgarson@gmail.com>
2021-05-01 22:24:12 +0100

    Repo and tailwind setup, welcome component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import { AppProps } from 'next/dist/next-server/lib/router/router'
+import React from 'react'
+import { AppProps } from 'next/app'
 import '../styles/globals.scss'
+import { Welcome } from '../components/welcome'
 
-const CustomApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return <>
+    <Welcome />
+    <Component {...pageProps} />
+  </>
 }
 
 export default CustomApp
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    colors: {
+      current: 'currentColor',
+      transparent: 'transparent',
+      red: 'var(--red)',
+    }
+  }
+}
