a3cfaba1c88f8390ab3bc49a7537e18d5be71a51
Sam Garson <samtgarson@gmail.com>
2021-05-22 22:19:05 +0100

    Fix mobile navbar layout

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -26,9 +26,15 @@ const navVariants = {
   wrapper: {
     initial: {
       opacity: 0,
-      transition: { staggerChildren: 0.1, staggerDirection: -1 }
+      transition: {
+        staggerChildren: 0.1,
+        staggerDirection: -1
+      }
     },
-    animate: { opacity: 1, transition: { staggerChildren: 0.1 } }
+    animate: {
+      opacity: 1,
+      transition: { staggerChildren: 0.1 }
+    }
   },
   item: {
     initial: {
@@ -83,14 +89,14 @@ const HomeButton: FC<{ artist: Artist }> = ({ artist }) => {
   return (
     <h1 className='flex items-center mr-auto overflow-hidden'>
       <Link href='/'>
-        <a className='block -ml-5 translate-x-0 hover:translate-x-5 transform transition-transform ease-out'>
+        <a className='block sm:-ml-5 translate-x-0 hover:translate-x-5 transform transition-transform ease-out'>
           <svg
             width='6'
             height='11'
             viewBox='0 0 6 11'
             fill='none'
             xmlns='http://www.w3.org/2000/svg'
-            className='inline-block mr-5'
+            className='sm:inline-block mr-5 hidden'
           >
             <path
               d='M5.14295 10.3799L0.997954 6.23488L0.292954 5.52988L0.997954 4.82488L5.14295 0.674878C5.23711 0.580726 5.3648 0.527832 5.49795 0.527832C5.6311 0.527832 5.7588 0.580726 5.85295 0.674878C5.94711 0.76903 6 0.896727 6 1.02988C6 1.16303 5.94711 1.29073 5.85295 1.38488L1.70295 5.52988L5.84795 9.67488C5.92987 9.77053 5.97267 9.89357 5.96781 10.0194C5.96295 10.1452 5.91078 10.2646 5.82174 10.3537C5.73269 10.4427 5.61332 10.4949 5.48748 10.4997C5.36164 10.5046 5.2386 10.4618 5.14295 10.3799Z'
@@ -134,8 +140,8 @@ export const NavBar: FC<{ artist: Artist, route: string }> = ({
         <section
           onClick={navClick}
           className={cn(
-            'relative py-6 px-5 sm:py-5 sm:px-6 mr-1 flex content-stretch sm:items-center flex-grow transition sm:h-full transform items-start flex-col sm:flex-row',
-            { 'opacity-0 -translate-y-2 pointer-events-none': hidden }
+            'relative py-5 px-5 sm:py-5 sm:px-6 mr-1 flex content-stretch sm:items-center flex-grow transition-opacity sm:h-full transform items-start flex-col sm:flex-row',
+            { 'opacity-0 pointer-events-none': hidden }
           )}
         >
           <span
@@ -145,7 +151,7 @@ export const NavBar: FC<{ artist: Artist, route: string }> = ({
               { 'h-18 sm:h-full closed': !open, 'h-full open': open }
             )}
           />
-          <div className='z-10 flex w-full sm:mr-auto sm:w-auto transition'>
+          <div className='z-10 flex items-center w-full sm:mr-auto sm:w-auto transition'>
             <HomeButton artist={artist} />
             <NavMenu
               className='block ml-auto sm:hidden'
